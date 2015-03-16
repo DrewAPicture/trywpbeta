81b078b54d2ec3bebfcb8f0b190f46bbe6a0ab0a
DrewAPicture <drew@10up.com>
2015-03-16 05:08:01 -0600

    Lockdown class + forced guest login quick pass.

---
+++ mu-plugins/lockdown.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Lockdown
+ * Version: 0.1
+ * License: GPLv2
+ */
+
+class Lockdown {
+
+	/**
+	 * Constructor.
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+		add_action( 'wp', 'force_guest_login' );
+	}
+
+	/**
+	 * Auto-login any visitors.
+	 *
+	 * @access public
+	 */
+	public function force_guest_login() {
+		if ( ! is_user_logged_in() ) {
+			wp_set_current_user( 2 );
+
+			if ( ! is_admin() ) {
+				if ( wp_redirect( admin_url() ) ) {
+					exit();
+				}
+			}
+		}
+	}
+}
+new Lockdown();
